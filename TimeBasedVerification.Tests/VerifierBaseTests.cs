bc6dbb43a0886ed62c33a50ad76ec11f3b996807
an-prata <evanrileyoverman@gmail.com>
2021-12-31 17:34:42 -0800

    test for code verification and proper disposing

--- TimeBasedVerification.Tests/VerifierBaseTests.cs
+++ TimeBasedVerification.Tests/VerifierBaseTests.cs
@@ -2,6 +2,7 @@
 // Copyright (c) 2021 Evan Overman (https://github.com/an-prata)
 // Licensed under the MIT License.
 
+using System;
 using System.Security.Cryptography;
 using Xunit;
 
@@ -23,5 +24,33 @@ public class VerifierBaseTests
 
 		Assert.Equal(preimage, decryptedCode);
 		Assert.NotEqual(verificationCode.Code, decryptedCodeBytes);
+	}
+
+	[Fact]
+	public void CodeVerificationTest()
+	{
+		const int keySize = 2048;
+		RSACryptoServiceProvider cryptoServiceProvider = new(keySize);
+		VerifierBase verifier0 = new(cryptoServiceProvider.ExportParameters(false), keySize, false);
+
+		VerificationCode verificationCode = verifier0.MakeVerificationCode();
+		verifier0.Dispose();
+
+		VerifierBase verifier1 = new(cryptoServiceProvider.ExportParameters(true), keySize, true);
+		Assert.True(verifier1.CheckVerificationCode(verificationCode));	// The Outcome of this may depend on the speed of execution.
+	}
+
+	[Fact]
+	public void DisposeTest()
+	{
+		const int keySize = 2048;
+		RSACryptoServiceProvider cryptoServiceProvider = new(keySize);
+		VerifierBase verifier = new(cryptoServiceProvider.ExportParameters(true), keySize, true);
+		VerificationCode verificationCode = verifier.MakeVerificationCode();
+		verifier.CheckVerificationCode(verificationCode);
+		verifier.Dispose();
+
+		Assert.Throws<ObjectDisposedException>(() => verifier.MakeVerificationCode());
+		Assert.Throws<ObjectDisposedException>(() => verifier.CheckVerificationCode(verificationCode));
 	}
 }
