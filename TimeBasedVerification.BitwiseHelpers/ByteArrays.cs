970f7e1043160383fe127ecd7551b9167d76c80c
an-prata <evanrileyoverman@gmail.com>
2022-02-03 21:38:18 -0800

    methods to make ulongs into byte[]s and vice versa

---
+++ TimeBasedVerification.BitwiseHelpers/ByteArrays.cs
@@ -0,0 +1,120 @@
+// TimeBasedVerification (https://github.com/an-prata/TimeBasedVerification)
+// Copyright (c) 2021 Evan Overman (https://github.com/an-prata)
+// Licensed under the MIT License.
+
+namespace TimeBasedVerification.BitwiseHelpers
+{
+	/// <summary>
+	/// Used to convert unsigned numerical types to byte arrays and vice versa.
+	/// </summary>
+	public static class ByteArrays
+	{
+		private const ushort byteMask16 = 0b_0000_0000_1111_1111;
+
+		private const uint byteMask32 = 0b_0000_0000_0000_0000_0000_0000_1111_1111;
+
+		private const ulong byteMask64 = 0b_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_1111_1111;
+
+		/// <summary>
+		/// Takes 16-bit ushort preimage and makes it a byte array.
+		/// </summary>
+		/// 
+		/// <param name="preimage">
+		/// A ushort to turn into a byte[].
+		/// </param>
+		/// 
+		/// <returns>
+		/// A byte array of length 2 so that index 0 is the first 8 bits of 
+		/// preimage and index 1 is the second 8 bits.
+		/// </returns>
+		public static byte[] ToBytes(ushort preimage)
+		{
+			byte[] imageBytes = new byte[2];
+
+            for (int i = 0; i < 2; i++)
+            {
+				// Gets the distance between the desired byte and it's first binary digit in bits.
+                int currentByte = 8 * i;
+
+				// Creates a mask in which the desired byte is 1111_1111.
+                ushort currentByteMask = (ushort)(byteMask16 << currentByte);
+
+				// Applies the mask so that image is set to 0 everywhere except the desired byte.
+                ushort image = (ushort)(preimage & currentByteMask);
+
+				// Shifts image so that the desired byte is first and adds it to the array.
+				imageBytes[i] = (byte)(image >> currentByte);
+            }
+
+			return imageBytes;
+		}
+
+		/// <summary>
+		/// Takes 32-bit uint preimage and makes it a byte array.
+		/// </summary>
+		/// 
+		/// <param name="preimage">
+		/// A uint to turn into a byte[].
+		/// </param>
+		/// 
+		/// <returns>
+		/// A byte array of length 4 so that index 0 is the first 8 bits of 
+		/// preimage and index 1 is the second 8 bits etc.
+		/// </returns>
+		public static byte[] ToBytes(uint preimage)
+		{
+			byte[] imageBytes = new byte[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+				// Gets the distance between the desired byte and it's first binary digit in bits.
+                int currentByte = 8 * i;
+
+				// Creates a mask in which the desired byte is 1111_1111.
+                uint currentByteMask = byteMask32 << currentByte;
+
+				// Applies the mask so that image is set to 0 everywhere except the desired byte.
+                uint image = preimage & currentByteMask;
+
+				// Shifts image so that the desired byte is first and adds it to the array.
+				imageBytes[i] = (byte)(image >> currentByte);
+            }
+
+			return imageBytes;
+		}
+
+		/// <summary>
+		/// Takes 64-bit ulong preimage and makes it a byte array.
+		/// </summary>
+		/// 
+		/// <param name="preimage">
+		/// A ulong to turn into a byte[].
+		/// </param>
+		/// 
+		/// <returns>
+		/// A byte array of length 8 so that index 0 is the first 8 bits of 
+		/// preimage and index 1 is the second 8 bits etc.
+		/// </returns>
+		public static byte[] ToBytes(ulong preimage)
+		{
+			byte[] imageBytes = new byte[8];
+
+            for (int i = 0; i < 8; i++)
+            {
+				// Gets the distance between the desired byte and it's first binary digit in bits.
+                int currentByte = 8 * i;
+
+				// Creates a mask in which the desired byte is 1111_1111.
+                ulong currentByteMask = byteMask64 << currentByte;
+
+				// Applies the mask so that image is set to 0 everywhere except the desired byte.
+                ulong image = preimage & currentByteMask;
+
+				// Shifts image so that the desired byte is first and adds it to the array.
+				imageBytes[i] = (byte)(image >> currentByte);
+            }
+
+			return imageBytes;
+		}
+	}
+}
---
+++ TimeBasedVerification.BitwiseHelpers/UnsignedNumericals.cs
@@ -0,0 +1,114 @@
+// TimeBasedVerification (https://github.com/an-prata/TimeBasedVerification)
+// Copyright (c) 2021 Evan Overman (https://github.com/an-prata)
+// Licensed under the MIT License.
+
+namespace TimeBasedVerification.BitwiseHelpers
+{
+	public static class UnsignedNumericals
+	{
+		/// <summary>
+		/// Takes a byte array of length 2 and makes a ushort.
+		/// </summary>
+		/// 
+		/// <param name="imageBytes">
+		/// The byte[] to turn into a ushort.
+		/// </param>
+		/// 
+		/// <returns>
+		/// A ushort made from the elements of imageBytes.
+		/// </returns>
+		public static ulong ToUshort(byte[] imageBytes)
+		{
+			if (imageBytes.Length != 2)
+			{
+				throw new ArgumentException("imageBytes was wrong length.");
+			}
+
+			ulong preimage = 0;
+
+			for (int i = 0; i < 2; i++)
+			{
+				// Gets the distance between the desired byte and it's first binary digit in bits.
+				int currentByte = i * 8;
+
+				// Gets the value of the desired byte and shifts it to the desired location.
+				ushort image = (ushort)(imageBytes[i] << currentByte);
+				
+				// Adds the desired byte to preimage.
+				preimage |= image;
+			}
+
+			return preimage;
+		}
+
+		/// <summary>
+		/// Takes a byte array of length 4 and makes a uint.
+		/// </summary>
+		/// 
+		/// <param name="imageBytes">
+		/// The byte[] to turn into a uint.
+		/// </param>
+		/// 
+		/// <returns>
+		/// A uint made from the elements of imageBytes.
+		/// </returns>
+		public static ulong ToUint(byte[] imageBytes)
+		{
+			if (imageBytes.Length != 4)
+			{
+				throw new ArgumentException("imageBytes was wrong length.");
+			}
+
+			ulong preimage = 0;
+
+			for (int i = 0; i < 4; i++)
+			{
+				// Gets the distance between the desired byte and it's first binary digit in bits.
+				int currentByte = i * 8;
+
+				// Gets the value of the desired byte and shifts it to the desired location.
+				uint image = ((uint)imageBytes[i]) << currentByte;
+				
+				// Adds the desired byte to preimage.
+				preimage |= image;
+			}
+
+			return preimage;
+		}
+
+		/// <summary>
+		/// Takes a byte array of length 8 and makes a ulong.
+		/// </summary>
+		/// 
+		/// <param name="imageBytes">
+		/// The byte[] to turn into a ulong.
+		/// </param>
+		/// 
+		/// <returns>
+		/// A ulong made from the elements of imageBytes.
+		/// </returns>
+		public static ulong ToUlong(byte[] imageBytes)
+		{
+			if (imageBytes.Length != 8)
+			{
+				throw new ArgumentException("imageBytes was wrong length.");
+			}
+
+			ulong preimage = 0;
+
+			for (int i = 0; i < 8; i++)
+			{
+				// Gets the distance between the desired byte and it's first binary digit in bits.
+				int currentByte = i * 8;
+
+				// Gets the value of the desired byte and shifts it to the desired location.
+				ulong image = ((ulong)imageBytes[i]) << currentByte;
+				
+				// Adds the desired byte to preimage.
+				preimage |= image;
+			}
+
+			return preimage;
+		}
+	}
+}
